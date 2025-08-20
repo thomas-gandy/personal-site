ad15ee5b94f1cd6250ce37e188f56e87dfdb5e96
Thomas Gandy <thomas@gandy.info>
2025-08-20 20:31:58 +0000

    feat: custom Mantine components for MDX

--- site/mdx-components.tsx
+++ site/mdx-components.tsx
@@ -1,6 +1,24 @@
+import { Anchor, AnchorProps, Code, CodeProps, List } from "@mantine/core";
 import type { MDXComponents } from "mdx/types";
+import Link from "next/link";
 
-const components: MDXComponents = {};
+interface CustomAnchorProps extends AnchorProps {
+  href: string;
+}
+
+function CustomAnchor(props: CustomAnchorProps) {
+  return <Anchor component={Link} {...props} fz={"inherit"} href={props.href} />;
+}
+
+function CodeBlock(props: CodeProps) {
+  return (
+    <Code {...props} block>
+      {props.children}
+    </Code>
+  );
+}
+
+const components: MDXComponents = { a: CustomAnchor, code: Code, pre: CodeBlock, ul: List };
 
 export function useMDXComponents(): MDXComponents {
   return components;
