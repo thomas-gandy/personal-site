b9b6babb0a856b702b5849449bfc836fea141165
Thomas Gandy <thomas@gandy.info>
2025-08-27 23:18:15 +0000

    feat: k8s controller concepts

---
+++ site/src/app/notes/k8s/controllers/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { HeroText } from "@/src/app/ui/HeroText";
+import { Container } from "@mantine/core";
+import Content from "./content.mdx";
+
+export default function Page() {
+  return (
+    <>
+      <HeroText>
+        <HeroText.Title>
+          Controllers in <HeroText.TitleHighlight>Kubernetes</HeroText.TitleHighlight>
+        </HeroText.Title>
+      </HeroText>
+      <Container size={"sm"} mt={"xl"} mb={"xl"}>
+        <Content />
+      </Container>
+    </>
+  );
+}
